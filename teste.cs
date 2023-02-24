c8c9c8285413cc5a6e259ba8838d5f3b1b2e3962
BuenoPereira <126211857+BuenoPereira@users.noreply.github.com>
2023-02-23 22:13:14 -0300

    Initial commit

---
+++ teste.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1
+{
+    internal class Teste
+    {
+        public static void metodo1(string a)
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
